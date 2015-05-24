e0cf74a38a4547900015d69a088556c9624f52a1
Sophie Kirschner <pineapplemachine@gmail.com>
2015-05-24 09:48:22 -0400

    Minor restructuring of manager code.

--- manager.py
+++ manager.py
@@ -28,36 +28,8 @@ def __main__():
         for script in settings.runscripts:
             pydwarf.log.debug('Handling script %s...' % script)
             
-            urist = None
-            scriptname = None
-            scriptfunc = None
-            scriptargs = None
-            if isinstance(script, tuple) or isinstance(script, list):
-                scriptargs = script[1]
-                script = script[0]
-            elif isinstance(script, dict):
-                scriptname = script.get('name')
-                scriptargs = script.get('args')
-                scriptmatch = script.get('match')
-                scriptignoreversion = script.get('ignore_df_version')
-                checkversion = None if scriptignoreversion else settings.dfversion
-                candidates = pydwarf.urist.get(scriptname, version=checkversion, match=scriptmatch)
-                if candidates and len(candidates):
-                    urist = candidates[0]
-                    scriptname = urist.name
-                    if len(candidates) > 1: pydwarf.log.warning('More than one fitting script has been specified, using a best guess.')                        
-            elif callable(script):
-                scriptname = script.__name__
-                scriptfunc = script
-            else:
-                scriptname = script
-                candidates = pydwarf.urist.get(scriptname, version=settings.dfversion)
-                if candidates and len(candidates):
-                    urist = candidates[0]
-                    scriptname = urist.name
-                    if len(candidates) > 1: pydwarf.log.warning('More than one fitting script has been specified, using a best guess.')
-            if urist and scriptfunc is None:
-                scriptfunc = urist.fn
+            urist, scriptname, scriptfunc, scriptargs = getscript(script)
+            
             
             if scriptfunc:
                 scriptinfo = 'Running script %s' % scriptname
@@ -90,5 +62,42 @@ def __main__():
     else:
         pydwarf.log.info('Specified raws directory does not exist.')
 
+def getscript(script):
+    # A script can be specified in a variety of ways in the scripts iterable, this function is for understanding all the different options and returning the info the manager needs.
+    urist = None
+    scriptname = None
+    scriptfunc = None
+    scriptargs = None
+    scriptmatch = None
+    scriptignoreversion = None
+    candidates = None
+    
+    if isinstance(script, basestring):
+        scriptname = script
+                
+    elif isinstance(script, dict):
+        scriptname = script.get('name')
+        scriptfunc = script.get('func')
+        scriptargs = script.get('args')
+        scriptmatch = script.get('match')
+        scriptignoreversion = script.get('ignore_df_version')
+        
+    elif callable(script):
+        scriptfunc = script
+        
+    if scriptname is not None:
+        checkversion = None if scriptignoreversion else settings.dfversion
+        candidates = pydwarf.urist.get(scriptname, version=checkversion, match=scriptmatch)
+        if candidates and len(candidates):
+            urist = candidates[0]
+            scriptname = urist.name
+            scriptfunc = urist.fn
+            if len(candidates) > 1: pydwarf.log.warning('More than one fitting script has been specified, using a best guess.')
+    elif scriptfunc is not None:
+        scriptname = script.__name__
+    
+    return urist, scriptname, scriptfunc, scriptargs
+
 if __name__ == "__main__":
     __main__()
+
