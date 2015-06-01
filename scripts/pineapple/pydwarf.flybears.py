2732105d07293e03a741f82bf7f18c9d9d957b73
Sophie Kirschner <sophie@thehumangeo.com>
2015-06-01 07:06:47 -0400

    Minor improvements to flybears and subplants scripts
    Mostly in the form of adding comments

--- scripts/pineapple/pydwarf.flybears.py
+++ scripts/pineapple/pydwarf.flybears.py
@@ -4,13 +4,18 @@ import pydwarf
     name = 'pineapple.flybears',
     version = 'alpha',
     author = 'Sophie Kirschner',
-    description = 'Causes all female bears to fly.',
+    description = 'Example script which causes all female bears to fly.',
     compatibility = (pydwarf.df_0_3x, pydwarf.df_0_40)
 )
 def flybears(raws):
+    # Get all bear creature tokens
     bears = raws.allobj('CREATURE', re_id='BEAR_.+')
+    
+    # Add [FLIER] to each of them, immediately after the first CASTE:FEMALE token
     for bear in bears:
         bear.get('CASTE:FEMALE').add('FLIER')
+        
+    # All done!
     if len(bears):
         return pydwarf.success('Made %d bear species fliers.' % len(bears))
     else:
--- scripts/pineapple/pydwarf.subplants.py
+++ scripts/pineapple/pydwarf.subplants.py
@@ -11,16 +11,30 @@ seasons = ('SPRING', 'SUMMER', 'AUTUMN', 'WINTER')
     compatibility = (pydwarf.df_0_34, pydwarf.df_0_40)
 )
 def subterraneanplants(dfraws):
+    # Get subterranean plants
     subplants = []
     for plant in dfraws.allobj('PLANT'):
         if plant.getuntil(pretty='BIOME:SUBTERRANEAN_WATER', until_exact_value='PLANT'):
             subplants.append(plant)
     if not len(subplants): return pydwarf.failure('Found no subterranean plants.')
+    
+    # Ensure each has all four seasons
     pydwarf.log.info('Found %d subterranean plants. Modifying...' % len(subplants))
+    modified = 0
     for subplant in subplants:
         pydwarf.log.debug('Handling %s...' % subplant)
-        for seasontoken in subplant.alluntil(value_in=seasons, until_exact_value='PLANT'):
-            seasontoken.remove()
-        for season in seasons:
-            subplant.add(raws.token(value=season))
-    return pydwarf.success('Made %d subterranean plants grow year-round.' % len(subplants))
+        seasontokens = subplant.alluntil(value_in=seasons, until_exact_value='PLANT')
+        if len(seasontokens) == 4:
+            # First remove the existing tokens
+            for seasontoken in seasontokens:
+                seasontoken.remove()
+            # Then add all four anew
+            for season in seasons:
+                subplant.add(raws.token(value=season))
+            modified += 1
+    
+    # All done
+    if modified > 0:
+        return pydwarf.success('Made %d subterranean plants grow year-round.' % modified)
+    else:
+        return pydwarf.failure('All subterranean plants already grew year-round.')
