8545eb64cb2c01c53442b68955112d41a8317f03
Sophie Kirschner <sophie@thehumangeo.com>
2015-06-01 08:17:29 -0400

    More cleanup of subplants script

--- scripts/pineapple/pydwarf.subplants.py
+++ scripts/pineapple/pydwarf.subplants.py
@@ -10,10 +10,10 @@ seasons = ('SPRING', 'SUMMER', 'AUTUMN', 'WINTER')
     description = 'Makes all subterranean plants grow year-round.',
     compatibility = (pydwarf.df_0_34, pydwarf.df_0_40)
 )
-def subterraneanplants(dfraws):
+def subterraneanplants(df):
     # Get subterranean plants
     subplants = []
-    for plant in dfraws.allobj('PLANT'):
+    for plant in df.allobj('PLANT'):
         if plant.getuntil(pretty='BIOME:SUBTERRANEAN_WATER', until_exact_value='PLANT'):
             subplants.append(plant)
     if not len(subplants): return pydwarf.failure('Found no subterranean plants.')
@@ -22,16 +22,18 @@ def subterraneanplants(dfraws):
     pydwarf.log.info('Found %d subterranean plants. Modifying...' % len(subplants))
     modified = 0
     for subplant in subplants:
-        pydwarf.log.debug('Handling %s...' % subplant)
-        seasontokens = subplant.alluntil(value_in=seasons, until_exact_value='PLANT')
-        if len(seasontokens) == 4:
-            # First remove the existing tokens
+        seasontokens = subplant.allprop(value_in=seasons)
+        if len(seasontokens) > 0 and len(seasontokens) < len(seasons):
+            pydwarf.log.debug('Adding season tokens to %s...' % subplant)
+            # First remove the existing tokens (To avoid making duplicates)
             for seasontoken in seasontokens:
                 seasontoken.remove()
             # Then add all four anew
             for season in seasons:
                 subplant.add(raws.token(value=season))
             modified += 1
+        else:
+            pydwarf.log.debug('Plant %s either has no seasonal tokens or already has all of them, skipping.' % subplant)
     
     # All done
     if modified > 0:
