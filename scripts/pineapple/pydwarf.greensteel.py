acf60fb9b1c92704116a026c8b72db65e01ed8d6
Sophie Kirschner <pineapplemachine@gmail.com>
2015-06-06 09:03:26 -0400

    pineapple.greensteel now adds PERMITTED_REACTION tokens

--- scripts/pineapple/pydwarf.greensteel.py
+++ scripts/pineapple/pydwarf.greensteel.py
@@ -3,6 +3,10 @@ import pydwarf
 
 greendir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'raws/greensteel')
 
+
+default_entities = ['MOUNTAIN']
+
+
 @pydwarf.urist(
     name = 'pineapple.greensteel',
     version = 'alpha',
@@ -10,13 +14,24 @@ greendir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'raws/greens
     description = '''Adds an alloy which is lighter and sharper than steel but not so much
         as adamantine. It can be made from similar ingredients as steel with the addition
         of adamantine bars or a new adamant ore.''',
+    arguments = {
+        'entities': '''The entities which should be permitted this reaction. Defaults to
+            only MOUNTAIN.'''
+    },
     compatibility = (pydwarf.df_0_3x, pydwarf.df_0_40)
 )
-def greensteel(df):
+def greensteel(df, entities=default_entities):
     # Add greensteel raws
     try:
         df.read(path=greendir, log=pydwarf.log)
-        return pydwarf.success()
+        return pydwarf.urist.getfn('pineapple.utils.addtoentity')(
+            df,
+            entities = entities,
+            permitted_reaction = (
+                'GREEN_STEEL_MAKING_ADAMANT_PINEAPPLE',
+                'GREEN_STEEL_MAKING_ADMANTINE_PINEAPPLE'
+            )
+        )
     except:
         pydwarf.log.exception('Failed to add greensteel raws.')
         return pydwarf.failure()
