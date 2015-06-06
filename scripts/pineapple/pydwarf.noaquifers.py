bd0874ab7ba74d2ddc37e44e26337dc453c98c73
Sophie Kirschner <pineapplemachine@gmail.com>
2015-06-06 14:49:14 -0400

    Added pineapple.noaquifers and pineapple.woodmechanisms

---
+++ scripts/pineapple/pydwarf.noaquifers.py
@@ -0,0 +1,16 @@
+import pydwarf
+
+@pydwarf.urist(
+    name = 'pineapple.noaquifers',
+    version = 'alpha',
+    author = 'Sophie Kirschner',
+    description = 'Removes all AQUIFER tokens.',
+    compatibility = (pydwarf.df_0_27, pydwarf.df_0_28, pydwarf.df_0_3x, pydwarf.df_0_40)
+)
+def noaquifers(df):
+    aquifers = df.all('AQUIFER')
+    if len(aquifers):
+        for aquifer in aquifers: aquifer.remove()
+        return pydwarf.success('Removed %d AQUIFER tokens.' % len(aquifers))
+    else:
+        return pydwarf.failure('Found no AQUIFER tokens.')
---
+++ scripts/pineapple/pydwarf.woodmechanisms.py
@@ -0,0 +1,41 @@
+import pydwarf
+
+
+
+wood_mechanisms_reaction = '''
+    [NAME:craft wooden mechanisms]
+    [BUILDING:CRAFTSMAN:NONE]
+    [REAGENT:A:%d:WOOD:NONE:NONE:NONE]
+    [PRODUCT:100:1:TRAPPARTS:NONE:GET_MATERIAL_FROM_REAGENT:A:NONE]
+    [SKILL:MECHANICS]
+'''
+
+default_log_count = 1
+
+default_entities = ['MOUNTAIN', 'PLAINS']
+
+default_file = 'reaction_woodmechanisms_pineapple'
+
+
+
+@pydwarf.urist(
+    name = 'pineapple.woodmechanisms',
+    version = 'alpha',
+    author = 'Sophie Kirschner',
+    description = '''Allows construction of wooden mechanisms at the craftdwarf's workshop.
+        Inspired by/stolen from Rubble's Wooden Mechanisms mod.''',
+    arguments = {
+        'log_count': 'The number of logs required in the reaction.',
+        'entities': 'Adds the reaction to these entities. Defaults to MOUNTAIN and PLAINS.',
+        'add_to_file': 'Adds the reaction to this file.'
+    },
+    compatibility = (pydwarf.df_0_2x, pydwarf.df_0_3x, pydwarf.df_0_40)
+)
+def woodmechanisms(df, log_count=default_log_count, entities=default_entities, add_to_file=default_file):
+    return pydwarf.urist.getfn('pineapple.utils.addreaction')(
+        df,
+        id = 'WOODEN_MECHANISMS_PINEAPPLE',
+        tokens = wood_mechanisms_reaction % log_count,
+        add_to_file = add_to_file,
+        permit_entities = entities
+    )
