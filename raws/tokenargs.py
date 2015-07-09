93007c8502b974628b23a56dca7fffba977616ee
Sophie Kirschner <pineapplemachine@gmail.com>
2015-07-09 19:04:00 -0400

    Improvements to raws.tokenargs

--- raws/tokenargs.py
+++ raws/tokenargs.py
@@ -10,6 +10,14 @@ class tokenargs(list):
         "']'": '93'
     }
     
+    def __init__(self, items):
+        if items is not None:
+            self.reset()
+        elif isinstance(items, basestring):
+            self.reset(items.split(':'))
+        else:
+            self.reset(items)
+    
     @staticmethod
     def sanitize(value):
         '''Internal: Utility method for sanitizing a string intended to be evaluated as an arugment for a token.'''
@@ -32,8 +40,19 @@ class tokenargs(list):
     def clear(self):
         del self[:]
         
-    def reset(self, items):
-        self[:] = (tokenargs.sanitize(item) for item in items)
+    def reset(self, items=None):
+        self[:] = (tokenargs.sanitize(item) for item in items) if items is not None else []
+        
+    def add(self, item):
+        if isinstance(item, basestring):
+            self.extend(item.split(':'))
+        elif hasattr(item, '__iter__') or hasattr(item, '__getitem__'):
+            self.extend(item)
+        else:
+            self.add(str(item))
+            
+    def sub(self, items):
+        self[:] = self[:-items]
         
     def __str__(self):
         return ':'.join(self)
@@ -44,6 +63,12 @@ class tokenargs(list):
     def __add__(self, items):
         return list.__add__(self, (tokenargs.sanitize(item) for item in items))
         
+    def __iadd__(self, item):
+        self.add(item)
+        
+    def __isub__(self, items):
+        self.sub(items)
+        
     def __contains__(self, item):
         try:
             san = tokenargs.sanitize(item)
@@ -51,9 +76,6 @@ class tokenargs(list):
             return False
         else:
             return list.__contains__(self, san)
-    
-    def __iadd__(self, item):
-        list.__iadd__(self, tokenargs.sanitize(item))
     
     def __init__(self, items=None):
         if items:
