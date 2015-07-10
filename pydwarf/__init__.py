4ed2994c7b9c4f20ccc2aeb7ad0526483cedc02a
Sophie Kirschner <sophie@thehumangeo.com>
2015-07-10 17:59:16 -0400

    Added some convenience functions to pydwarf __init__
    Todo: Maybe put these in their own module though?

--- pydwarf/__init__.py
+++ pydwarf/__init__.py
@@ -29,3 +29,27 @@ from response import *
 from urist import *
 from config import *
 from helpers import *
+
+
+
+def quick(raws, root=None, **kwargs):
+    se = session()
+    if root is not None: kwargs['root'] = root
+    args = {
+        'input': 'df',
+        'paths': 'auto',
+        'version': 'auto',
+        'hackversion': 'auto',
+        'output': 'output/',
+        'backup': None,
+        'packages': 'scripts',
+        'verbose': True,
+        'log': '',
+    }
+    args.update(kwargs)
+    se.load(raws, args=args)
+    return se
+
+def df(*args, **kwargs):
+    se = quick(*args, **kwargs)
+    return se.df
