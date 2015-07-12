bb15f2ab49d71a4aacd539ccf02efb0a07c77b1c
Sophie Kirschner <pineapplemachine@gmail.com>
2015-07-12 08:11:24 -0400

    Fixed methods in raws.queryableadd

--- raws/queryableadd.py
+++ raws/queryableadd.py
@@ -13,31 +13,31 @@ class rawsqueryableadd(rawsqueryable):
     def set(self, *args, **kwargs):
         value, args = rawsqueryableadd.argsset(*args, **kwargs)
         settoken = self.get(exact_value=value)
-        if not settoken:
+        if settoken is None:
             return self.add(value=value, args=args)
         else:
-            settoken.setargs(args)
+            settoken.args.reset(args)
             return settoken
             
     def setprop(self, *args, **kwargs):
         value, args = rawsqueryableadd.argsset(*args, **kwargs)
         settoken = self.getprop(exact_value=value)
-        if not settoken:
+        if settoken is None:
             return self.addprop(value=value, args=args)
         else:
-            settoken.setargs(args)
+            settoken.args.reset(args)
             return settoken
             
     def setall(self, *args, **kwargs):
         value, args = rawsqueryableadd.argsset(*args, **kwargs)
         settokens = self.all(exact_value=value)
-        settokens.each(lambda token: token.setargs(args))
+        settokens.each(lambda token: token.args.reset(args))
         return settokens
             
     def setallprop(self, *args, **kwargs):
         value, args = rawsqueryableadd.argsset(*args, **kwargs)
         settokens = self.allprop(exact_value=value)
-        settokens.each(lambda token: token.setargs(args))
+        settokens.each(lambda token: token.args.reset(args))
         return settokens
         
     @staticmethod
