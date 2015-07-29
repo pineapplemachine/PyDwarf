1074063142376c22a85b4006520a149580a49f17
Sophie Kirschner <sophie@thehumangeo.com>
2015-07-29 07:12:10 -0400

    Better documentation for raws.helpers
    Also, now copytokens(iter=True) returns a tokengenerator

--- raws/helpers.py
+++ raws/helpers.py
@@ -5,6 +5,7 @@ import textwrap
 
 
 def copy(item, *args, **kwargs):
+    '''Copy a raws object.'''
     if hasattr(item, 'copy'):
         return item.copy(*args, **kwargs)
     elif inspect.isgenerator(item) or inspect.isgeneratorfunction(item):
@@ -13,6 +14,7 @@ def copy(item, *args, **kwargs):
         return lcopytokens(item, *args, **kwargs)
         
 def equal(a, b): 
+    '''Check for equivalency between two raws objects.'''
     if hasattr(a, 'equals'):
         return a.equals(b)
     elif hasattr(b, 'equals'):
@@ -36,32 +38,35 @@ def ends(tokens, setfile=None):
         return first, last
         
 def tokensstring(tokens, dedent=True):
-        if len(tokens) == 0:
-            return ''
-        elif len(tokens) == 1:
-            return str(tokens[0])
-        else:
-            parts = []
-            minindent = None
-            for token in tokens:
-                prefix = ''
-                text = str(token)
-                suffix = ''
-                if ((token.prefix and '\n' in token.prefix)): prefix += '\n'
-                if token.prefix: prefix += token.prefix.split('\n')[-1]
-                if token.suffix: suffix += token.suffix.split('\n')[0]
-                if ((token.suffix and '\n' in token.suffix)): suffix += '\n'
-                parts.extend((prefix, text, suffix))
-            fulltext = ''.join(parts).strip('\n')
-            if dedent: fulltext = textwrap.dedent(fulltext)
-            return fulltext
+    '''Get a fancy string representation of some collection of tokens.'''
+    if len(tokens) == 0:
+        return ''
+    elif len(tokens) == 1:
+        return str(tokens[0])
+    else:
+        parts = []
+        minindent = None
+        for token in tokens:
+            prefix = ''
+            text = str(token)
+            suffix = ''
+            if ((token.prefix and '\n' in token.prefix)): prefix += '\n'
+            if token.prefix: prefix += token.prefix.split('\n')[-1]
+            if token.suffix: suffix += token.suffix.split('\n')[0]
+            if ((token.suffix and '\n' in token.suffix)): suffix += '\n'
+            parts.extend((prefix, text, suffix))
+        fulltext = ''.join(parts).strip('\n')
+        if dedent: fulltext = textwrap.dedent(fulltext)
+        return fulltext
         
 
 
 def copytokens(tokens, iter=False):
-    return icopytokens(tokens) if iter else lcopytokens(tokens)
+    '''Copy tokens contained within some iterable.'''
+    return tokengenerator.tokengenerator(icopytokens(tokens)) if iter else lcopytokens(tokens)
     
 def lcopytokens(tokens):
+    '''Internal: Copy tokens as a tokenlist.'''
     copiedtokens = tokenlist.tokenlist()
     prevtoken = None
     for sourcetoken in tokens:
@@ -73,6 +78,7 @@ def lcopytokens(tokens):
     return copiedtokens
     
 def icopytokens(tokens):
+    '''Internal: Copy tokens as a generator.'''
     prevtoken = None
     for sourcetoken in tokens:
         copytoken = sourcetoken.copy()
@@ -84,6 +90,7 @@ def icopytokens(tokens):
 
 
 def tokensequal(atokens, btokens):
+    '''Check equivalency between two iterables containing tokens.'''
     return all(
         atoken.equals(btoken) for atoken, btoken in itertools.izip_longest(
             atokens, btokens, fillvalue=token.token.nulltoken
@@ -94,3 +101,4 @@ def tokensequal(atokens, btokens):
 
 import token
 import tokenlist
+import tokengenerator
