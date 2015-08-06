ccfb8de8a0bfb9abd7a94c44d530711aa2dd9f4a
Sophie Kirschner <pineapplemachine@gmail.com>
2015-08-06 18:59:08 -0400

    Improved smeeprocket.transgender

--- scripts/smeeprocket/pydwarf.transgender.py
+++ scripts/smeeprocket/pydwarf.transgender.py
@@ -57,23 +57,31 @@ add_sterile_interaction = '''
     },
     compatibility = pydwarf.df_0_40
 )
-def trans(dfraws, species=default_species, beards=True, frequency=500):
+def trans(df, species=default_species, beards=True, frequency=500):
     # Add new interaction
-    pydwarf.log.debug('Adding sterility interaction...')
-    objinteraction = dfraws.get('OBJECT:INTERACTION')
-    if objinteraction:
-        objinteraction.add(pretty=add_sterile_interaction)
+    response = pydwarf.scripts.pineapple.utils.addobject(
+        df,
+        add_to_file = 'raw/objects/smeeprocket_transgender_interaction.txt',
+        tokens = add_sterile_interaction
+    )
+    if response:
+        pydwarf.log.debug('Added sterility interaction.')
     else:
         return pydwarf.failure('Unable to add sterility interaction.')
     
     # Add new castes
     castefailures = []
-    creaturetokens = dfraws.allobj(type='CREATURE', id_in=species)
+    creaturetokens = df.allobj(type='CREATURE', id_in=species)
     for creaturetoken in creaturetokens:
         pydwarf.log.debug('Handling creature %s...' % creaturetoken)
         
         castes = creaturetoken.allprop(exact_value='CASTE', args_count=1)
-        if len(castes) == 2 and ((castes[0].args[0] == 'MALE' and castes[1].args[0] == 'FEMALE') or (castes[1].args[0] == 'MALE' and castes[0].args[0] == 'FEMALE')):
+        if(
+            len(castes) == 2 and (
+                (castes[0].args[0] == 'MALE' and castes[1].args[0] == 'FEMALE') or
+                (castes[1].args[0] == 'MALE' and castes[0].args[0] == 'FEMALE')
+            )
+        ):
             
             # Remove DESCRIPTION token from the creature and add it to each caste
             descriptiontoken = creaturetoken.get(exact_value='DESCRIPTION', args_count=1)
