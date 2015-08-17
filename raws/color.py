d2eb22ff0df6436eea430929a0622825ca1dc32d
Sophie Kirschner <pineapplemachine@gmail.com>
2015-08-17 13:39:40 -0400

    Cleaned and added new functionality to raws.color

--- raws/color.py
+++ raws/color.py
@@ -19,28 +19,108 @@
     raws.color.lblue(pydwarf.color.lred) == (1, 12, 1)
 '''
 
-class color:
-    def __init__(self, value):
+class record:
+    '''
+        Instances record name and index information associated with each of
+        Dwarf Fortress' sixteen colors.
+    '''
+    
+    def __init__(self, name, value):
+        '''Initialize a color record.'''
+        self.name = name
         self.value = value
-    def __call__(self, bg=0, i=0):
-        return (str(self.value % 8), str(int(bg)), str(int(i or (self.value >= 8))))
-    def __int__(self):
-        return self.value
-    def __str__(self):
-        return str(self.value)
-    def __repr__(self):
-        return str(self)
         
+    def __call__(self, *args, **kwargs):
+        '''Get token arguments representing this color as the foreground.'''
+        return self.fg(*args, **kwargs)
+        
+    def __int__(self):
+        '''Get the color index associated with the record.'''
+        return self.value
+        
+    def __str__(self):
+        '''Get the name of the color record.'''
+        return self.name
+        
+    def fg(self, bg=0, i=0):
+        '''Get token arguments representing this color as the foreground.'''
+        return tokenargs.tokenargs((
+            self.value % 8,
+            int(bg),
+            int(i or (self.value >= 8))
+        ))
     def bg(self):
-        return ('0', str(self.value), '0')
+        '''Get token arguments representing this color as the background.'''
+        return tokenargs.tokenargs(('0', str(self.value), '0'))
         
     # Overload common operators
-    def __eq__(self, value): return self.value == value
-    def __ne__(self, value): return self.value != value
-    def __add__(self, value): return self.value + value
-    def __sub__(self, value): return self.value - value
-    def __mul__(self, value): return self.value * value
-    def __div__(self, value): return self.value / value
-    def __mod__(self, value): return self.value % value
+    
+    def __eq__(self, value):
+        return self.value == int(value)
+    def __ne__(self, value):
+        return self.value != int(value)
+    def __add__(self, value):
+        return self.value + int(value)
+    def __sub__(self, value):
+        return self.value - int(value)
+    def __mul__(self, value):
+        return self.value * int(value)
+    def __div__(self, value):
+        return self.value / int(value)
+    def __mod__(self, value):
+        return self.value % int(value)
 
-black, blue, green, cyan, red, magenta, brown, lgray, dgray, lblue, lgreen, lcyan, lred, lmagenta, yellow, white = [color(i) for i in xrange(0, 16)]
+
+
+def __init__():
+    '''Internal: Initialize color records.'''
+    
+    names = (
+        'black', 'blue', 'green', 'cyan',
+        'red', 'magenta', 'brown', 'lgray',
+        'dgray', 'lblue', 'lgreen', 'lcyan',
+        'lred', 'lmagenta', 'yellow', 'white',
+    )
+
+    get.colorlist = []
+    for index, name in enumerate(names):
+        col = record(name, index)
+        get.colorlist.append(col)
+        globals()[name] = col
+
+
+
+def get(value, i=0):
+    '''Get a color record by value and optional intensity.'''
+    value = int(value)
+    if i: value += 8
+    return get.colorlist[value]
+    
+def fg(args):
+    '''Extract foreground color record from a set of three token arguments.'''
+    if isinstance(args, token.token): args = args.args
+    return get(args[0], args[2])
+    
+def bg(args):
+    '''Extract background color record from a set of three token arguments.'''
+    if isinstance(args, token.token): args = args.args
+    return get(args[1])
+    
+def group(fg=None, bg=None):
+    '''
+        Combine two color records to get a single list of foreground,
+        background, intensity arguments.
+    '''
+    f = fg() if fg is not None else (0, 0, 0)
+    return tokenargs.tokenargs((fg.value % 8, bg.value, int(fg.value >= 8)))
+
+
+
+__init__()
+
+list = get.colorlist
+
+
+
+import tokenargs
+import token
