934684ee12ebfc58c217c1cc8e7205657888f1bf
Sophie Kirschner <pineapplemachine@gmail.com>
2017-05-13 10:35:37 -0400

    Added pineapple.playanimalmen script

---
+++ scripts/pineapple/pydwarf.playanimalmen.py
@@ -0,0 +1,25 @@
+import pydwarf
+
+@pydwarf.urist(
+    name = 'pineapple.playanimalmen',
+    title = 'Animal Men Playable in Adventure Mode',
+    version = '1.0',
+    author = 'Sophie Kirschner',
+    description = '''
+        Makes all animal men playable in adventure mode by adding a
+        [OUTSIDER_CONTROLLABLE] token to each.''',
+)
+def playanimalmen(df):
+    added = set()
+    
+    for animalman in df.allobj('CREATURE', re_id='.+MAN'):
+        needprops = ('LARGE_ROAMING', 'CAN_LEARN', 'CAN_SPEAK')
+        if(
+            animalman.getprop('APPLY_CREATURE_VARIATION:ANIMAL_PERSON') or
+            all(animalman.getprop(prop) for prop in needprops)
+        ):
+            animalman.addprop('OUTSIDER_CONTROLLABLE')
+            added.add(animalman.args[0])
+    
+    # All done, now let PyDwarf know the script did its job successfully.
+    return pydwarf.success('Added OUTSIDER_CONTROLLABLE tokens to %d creatures.' % len(added))
